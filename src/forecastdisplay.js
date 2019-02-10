eebbb2b1b3545a2ded6fea2f8240ba26fe2f7471
louis-pham <l.c.pham@hotmail.com>
2019-02-11 03:16:26 +0900

    Completed render for forecast display (chartjs line graphs); added Toronto forecast; refactored ForecastDisplay to different file;

---
+++ src/forecastdisplay.js
@@ -0,0 +1,144 @@
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+import { APPID } from './appid.js';
+
+const API = "https://api.openweathermap.org/data/2.5/forecast?units=metric&appid=" + APPID;
+
+class ForecastDisplay extends React.Component {
+  constructor(props) {
+    super(props);
+    this.timeZone = props.timeZone;
+    this.cityName = props.cityName;
+    this.cityId = props.cityId;
+    this.state = {
+      isLoaded: false,
+      data: null,
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    const callUrl = API + "&id=" + this.cityId;
+    fetch(callUrl)
+      .then(response => response.json())
+      .then(
+        data => {
+          this.setState({
+            isLoaded: true,
+            data: data.list,
+          });
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          })
+        });
+  }
+
+  render() {
+    let toRender = [<h1 key={this.cityName}>{this.cityName}</h1>];
+    if (this.state.isLoaded) {
+      let temperatures = [];
+      let labels = [];
+      let timeZoneName;
+      for (let i=0; i<=12;i++ ) { // 36hr forecast
+        const { dt, main, weather } = this.state.data[i];
+        const { temp } = main;
+        const { icon } = weather[0];
+        if (i === 0) {
+          // get short version of timezone
+          timeZoneName = Intl.DateTimeFormat('en-US',
+            {
+              timeZone: this.timeZone,
+              timeZoneName: "short"
+            }).format().split(",")[1];
+        }
+        // let weatherIcon = "http://openweathermap.org/img/w/" + icon + ".png";
+
+        let dateTime = new Date(dt * 1000).toLocaleString("en-US", {timeZone: this.timeZone});
+        dateTime = new Date(dateTime);
+        let hours = dateTime.getHours();
+        const amOrPm = hours >= 12 ? 'PM' : 'AM';
+        hours = hours % 12;
+        hours = hours ? hours : 12;
+
+        labels.push(dateTime.getMonth() + 1 + "/" + dateTime.getDate() + " " + hours + ":00" + amOrPm);
+        temperatures.push(temp);
+
+      }
+
+      const options = {
+        legend: {
+          display: false,
+        },
+        scales: {
+          yAxes: [{
+            scaleLabel: {
+              display: true,
+              labelString: "Temperature (C)",
+              fontColor: "#fff",
+            },
+            ticks: {
+              fontColor: "#fff",
+            },
+            gridLines: {
+              color: "#2b2b2b",
+              zeroLineColor: "#fff",
+            },
+          }],
+          xAxes: [{
+            scaleLabel: {
+              display: true,
+              labelString: "Time (timezone: " + timeZoneName + ")",
+              fontColor: "#fff",
+            },
+            ticks: {
+              fontColor: "#fff",
+            },
+          }],
+        },
+      };
+
+      let sortaYellow = 'rgba(255,226,0,1)';
+      let white = 'rgba(255,255,255,1)';
+
+      toRender.push(
+        <Line
+          key={"f2"}
+          height={60}
+          data={
+            {
+              labels: labels,
+              datasets: [
+                {
+                  label: "Temperature (C)",
+                  lineTension: 0.25,
+                  pointRadius: 5,
+                  pointHoverRadius: 7,
+                  fill: false,
+                  backgroundColor: sortaYellow,
+                  borderColor: sortaYellow,
+                  pointBackgroundColor: white,
+                  pointStrokeColor: '#fff',
+                  pointHighlightFill: '#fff',
+                  pointHighlightStroke: sortaYellow,
+                  data: temperatures,
+                }
+              ]
+            }
+          }
+          options={options}
+        />
+      );
+    } else {
+      toRender.push(<div key={"loading"}>Loading...</div>);
+    }
+
+    return (
+      <div className="forecast-display">{toRender}</div>
+    );
+  }
+}
+
+export default ForecastDisplay;
--- src/index.js
+++ src/index.js
@@ -1,158 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { APPID } from './appid.js';
-import { Line } from 'react-chartjs-2';
-
-const API = "https://api.openweathermap.org/data/2.5/forecast?units=metric&appid=" + APPID;
-
-function ForecastDisplay(props) {
-
-  const { list } = props.data;
-  const { name: cityName, id: cityId } = props.data.city;
-  let contents = [
-    <h1 key={cityId}>{cityName}</h1>
-  ];
-  let forecasts = [];
-  let temperatures = [];
-
-  for (let i=0; i<=8;i++ ) { // 24hr forecast
-    let { dt, main, weather } = list[i];
-    let dateTime = new Date(dt * 1000);
-    let { temp } = main;
-    let { main: weatherMain, description, icon } = weather[0];
-    let weatherIcon = "http://openweathermap.org/img/w/" + icon + ".png";
-
-    temperatures.push(temp);
-
-    forecasts.push({
-      key: i,
-      dateTime: dateTime,
-      temperature: temp,
-      weather: weatherMain,
-      description: description,
-      icon: weatherIcon,
-    });
-    contents.push(
-      <div key={i}>
-        <img src={weatherIcon} style={{float: "right"}}></img>
-        <ul>
-          <li>Datetime: {dateTime.toString()}</li>
-          <li>Weather:
-            <ul>
-              <li>{weatherMain} -- {description}</li>
-              <li>Current: {temp}</li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-    );
-  }
-
-  const options = {
-    scales: {
-      yAxes: [{
-        scaleLabel: {
-          display: true,
-          labelString: "Temperature"
-        },
-      }],
-      xAxes: [{
-        scaleLabel: {
-          display: true,
-          labelString: "Time"
-        },
-      }],
-    },
-
-  };
-
-  contents.push(
-    <Line
-      key={"f2"}
-      height={"80"}
-      data={
-        {
-          labels: ["never", "gonna", "give", "you", "up","never", "gonna", "let", "you", "down"],
-          datasets: [
-            {
-              label: "Temperature",
-              lineTension: 0.25,
-              fill: false,
-              backgroundColor: 'rgba(151,187,205,1)',
-              borderColor: 'rgba(151,187,205,1)',
-              pointBackgroundColor: 'rgba(151,187,205,1)',
-              pointStrokeColor: '#fff',
-              pointHighlightFill: '#fff',
-              pointHighlightStroke: 'rgba(151,187,205,1)',
-              data: temperatures,
-            }
-          ]
-        }
-      }
-      options={options}
-    />
-  );
-  return contents;
-}
+import ForecastDisplay from './forecastdisplay.js';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       cities: [
-        { name: "Yokohama", id: "1848354", },
-        { name: "Toronto", id: "6167865", },
+        { name: "Yokohama", id: "1848354", timeZone: "Asia/Tokyo", },
+        { name: "Toronto", id: "6167865", timeZone: "America/Toronto", },
       ],
-      isLoaded: false,
-      data: null,
-      error: null,
     };
   }
 
-  componentDidMount() {
-    const cityId = this.state.cities[0].id
-    const callUrl = API + "&id=" + cityId;
-    fetch(callUrl)
-      .then(response => response.json())
-      .then(
-        data => {
-          this.setState({
-            isLoaded: true,
-            data: data,
-          });
-        },
-        error => {
-          this.setState({
-            isLoaded: true,
-            error
-          })
-        });
-  }
-
   render() {
-    const { error, isLoaded, data } = this.state;
-    if (error) {
-      return (
-        <div>
-          <h3>oopsie</h3>
-          {JSON.stringify(error)}
-        </div>
-      );
-    }
-    if (isLoaded) {
-      return ([
-        <ForecastDisplay
-          key={"f1"}
-          data={data}
-        />,
-      ]
-      );
-    } else {
-      return (
-        <div>Loading...</div>
-      );
-    }
+    return ([
+      <ForecastDisplay
+        key={this.state.cities[0].id}
+        cityId={this.state.cities[0].id}
+        cityName={this.state.cities[0].name}
+        timeZone={this.state.cities[0].timeZone}
+      />,
+      <ForecastDisplay
+        key={this.state.cities[1].id}
+        cityId={this.state.cities[1].id}
+        cityName={this.state.cities[1].name}
+        timeZone={this.state.cities[1].timeZone}
+      />,
+    ]
+    );
   }
 }
 
